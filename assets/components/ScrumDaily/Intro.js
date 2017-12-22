9258b0c6b21ca227e81e2a47197fcc10daeb9f9a
Oleksandr Rudin <Oleksandr.Rudin@3shape.com>
2017-12-22 15:23:32 +0200

    added fonts

--- assets/components/ScrumDaily/Intro.js
+++ assets/components/ScrumDaily/Intro.js
@@ -1,35 +1,13 @@
 import React from 'react'
-import firebase from '../../firebase/db'
-import RaisedButton from 'material-ui/RaisedButton'
-import { Link } from 'react-router-dom'
-import ReactDOM from 'react-dom'
-import Loader from 'react-loaders'
 
 export default class Intro extends React.Component {
-    state = {
-        loaded: false,
-    }
-    componentDidMount() {
-        let image = ReactDOM.findDOMNode(this.refs.img)
-        firebase.storage().ref().child('daily-intro.jpg').getDownloadURL().then(url => {
-            image.src = url
-            image.onload = () => this.setState({ loaded: true })
-        })
-    }    
     nextStep = () => {
         this.props.history.push('/daily/shuffling')
     }
     render() {
-        const { loaded } = this.state
         return (
-            <div className="text-center"> 
-                    <div className="overlay"></div>
-                    <Loader active={!loaded} type="ball-clip-rotate-multiple" />
-                    <img 
-                        onClick={this.nextStep}
-                        ref="img"
-                        className={loaded ? "image image-loaded fullpage-pic" : 'image'}
-                        alt="Daily Intro"/>
+            <div className="text-wrapper" onClick={this.nextStep}> 
+                <div className="daily-text">Silhouette Daily</div>
             </div>
         )
     }
