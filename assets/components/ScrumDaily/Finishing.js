63fd026514677d23dca7eaabeea61866b0ab190c
sFiFty <alexandr.rudin.my@gmail.com>
2017-12-23 20:14:00 +0200

    added fonts

--- assets/components/ScrumDaily/Finishing.js
+++ assets/components/ScrumDaily/Finishing.js
@@ -1,35 +1,13 @@
 import React from 'react'
-import firebase from '../../firebase/db'
-import RaisedButton from 'material-ui/RaisedButton'
-import { Link } from 'react-router-dom'
-import ReactDOM from 'react-dom'
-import Loader from 'react-loaders'
 
-export default class Intro extends React.Component {
-    state = {
-        loaded: false,
-    }
-    componentDidMount() {
-        let image = ReactDOM.findDOMNode(this.refs.img)
-        firebase.storage().ref().child('daily-finish.jpg').getDownloadURL().then(url => {
-            image.src = url
-            image.onload = () => this.setState({ loaded: true })
-        })
-    }    
+export default class Finishing extends React.Component {
     nextStep = () => {
         this.props.history.push('/')
     }
     render() {
-        const { loaded } = this.state
         return (
-            <div className="text-center"> 
-                    <div className="overlay"></div>
-                    <Loader active={!loaded} type="ball-clip-rotate-multiple" />
-                    <img 
-                        onClick={this.nextStep}
-                        ref="img"
-                        className={loaded ? "image image-loaded fullpage-pic" : 'image'}
-                        alt="Daily Finishing"/>
+            <div className="text-wrapper" onClick={this.nextStep}> 
+                <div className="daily-text">Let's back to work</div>
             </div>
         )
     }
