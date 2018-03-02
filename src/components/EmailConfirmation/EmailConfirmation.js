a9b84dc65928f6ac9feba6f073781a1089cc4255
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-03-02 12:25:06 +0200

    started working on email confirmation

--- src/components/EmailConfirmation/EmailConfirmation.js
+++ src/components/EmailConfirmation/EmailConfirmation.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import Moment from 'react-moment'
+import {Container} from 'semantic-ui-react'
 
 export default class EmailConfirmation extends React.Component {
 	render() {
 		const {firebase, history, profile} = this.props
+		console.log(this.props)
 		firebase.auth().onAuthStateChanged(user => {
-			if (!user) {
+			console.log(user)
+			if (!user || user.emailVerified) {
 				history.push('/')
 				return
 			}
 			user.sendEmailVerification()
 		})
 		return (
-			<div> 
+			<Container> 
 				<h1>Verify Your Email Address</h1>
 				<div>
 					We've sent an email to {profile.email} to verify your address. Please click the link in that email to continue.
 				</div>
-			</div>
+			</Container>
 		)
 	}
 }
