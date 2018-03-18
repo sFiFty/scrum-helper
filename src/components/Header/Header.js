7491c03d457d604318a8b076607d4b24cc47cd26
sFiFty <alexandr.rudin.my@gmail.com>
2018-03-18 21:35:26 +0200

    set logo as a link to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Container, Image, Label} from 'semantic-ui-react'
 import Navigation from 'Components/Navigation'
+import {Link} from 'react-router-dom'
 import Auth from 'Components/Auth'
 import './styles.scss'
 
@@ -14,9 +15,12 @@ export default class Header extends React.Component {
           <div className="d-flex justify-content-end">
             <div className="p-2">
               <div className="logo-container">
+
                 <Image
+                  as={Link}
                   alt="Scrum Helper"
                   title="Scrum Helper"
+                  to="/"
                   src={require('Images/logo.png')}
                 />
                 <Label className="beta-label" size="mini" color='black' ribbon>BETA</Label>
