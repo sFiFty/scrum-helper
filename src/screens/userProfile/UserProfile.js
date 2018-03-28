4454c8bf2d1380c28cd2b20e2ab60847fe26e313
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-03-28 09:55:58 +0300

    added dropzone made possible to change user avatar

--- src/screens/userProfile/UserProfile.js
+++ src/screens/userProfile/UserProfile.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Container, Icon, Image} from 'semantic-ui-react'
+import {Container, Icon, Image, Button} from 'semantic-ui-react'
 import {isLoaded} from 'react-redux-firebase'
+import Dropzone from 'react-dropzone'
 import SMLoader from 'Components/SMLoader'
 import './styles.scss'
 
 export default class UserProfile extends React.Component {
+
+	onFileAdded = (files, rejected) => {
+		const {firebase} = this.props
+		return firebase.uploadFiles('test', files, 'test').then(data => {
+			firebase.updateProfile({avatar: data[0].File.downloadURL})
+		})
+	}
+
 	render() {
 		const {profile} = this.props
+		console.log(profile);
+		let dropzoneRef
 		return (
 			!isLoaded(profile)
 			? <SMLoader />
 			:   
 			<Container>
 				<div className="d-flex flex-row profile-container justify-content-center align-items-center">
-					<div className="image-container">
-						<Image size="medium" src={profile.avatar} />
+					<div className="image-container d-flex flex-column align-items-center justify-content-center">
+						<Dropzone 
+							style={{ borderWidth: 0 }}
+							ref={node => dropzoneRef = node} 
+							onDrop={this.onFileAdded}>
+							<Image src={profile.avatar} />
+						</Dropzone>
+						<Button onClick={() => { dropzoneRef.open() }} className="mt-3 w-100" basic size="tiny">Change Avatar</Button>
 					</div>
 					<div className="profile-information-container">
 						<h1>{profile.name}</h1>
