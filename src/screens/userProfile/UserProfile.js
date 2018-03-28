2093f519f43c3c058b2d4326178bb0ba76a46737
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-03-28 08:36:10 +0300

    changed styles for profile page

--- src/screens/userProfile/UserProfile.js
+++ src/screens/userProfile/UserProfile.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Container, Icon, Image} from 'semantic-ui-react'
+import {isLoaded} from 'react-redux-firebase'
 import SMLoader from 'Components/SMLoader'
-import {isLoaded, isEmpty} from 'react-redux-firebase'
-import {Card, Icon, Image} from 'semantic-ui-react'
+import './styles.scss'
 
 export default class UserProfile extends React.Component {
 	render() {
-		const { profile } = this.props
+		const {profile} = this.props
 		return (
 			!isLoaded(profile)
 			? <SMLoader />
 			:   
-			<Card>
-				<Image size="medium" src={profile.avatar} />
-				<Card.Content>
-					<Card.Header>
-						{profile.displayName}
-					</Card.Header>
-					<Card.Meta>
-						Verification type: {profile.verificationType}
-					</Card.Meta>
-				</Card.Content>
-				<Card.Content extra>
-					Email: {profile.email}
-				</Card.Content>
-			</Card>
+			<Container>
+				<div className="d-flex flex-row profile-container justify-content-center align-items-center">
+					<div className="image-container">
+						<Image size="medium" src={profile.avatar} />
+					</div>
+					<div className="profile-information-container">
+						<h1>{profile.name}</h1>
+						<div className="profile-email">
+							<Icon name="mail" /> {profile.email}
+						</div>
+					</div>
+				</div>
+			</Container>
 		)
 	}
 	
