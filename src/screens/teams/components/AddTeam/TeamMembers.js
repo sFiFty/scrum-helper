41db48612cd4a146c22a2e89209194ff765e23b7
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-05 07:44:08 +0300

    moved team members list to separate table

---
+++ src/screens/teams/components/AddTeam/TeamMembers.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {List, Image, Icon} from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+
+export default class TeamList extends React.Component {
+	render() {
+		const {members, removeMember} = this.props
+		return (
+      members &&
+      <List className="w-50 generated-members-list">
+        {
+          members.map((member, key) => {
+            return (
+              <List.Item key={key} className="member d-flex justify-content-start align-items-center">
+                <Image avatar src={require(`Images/${member.avatar}`)} />
+                <List.Content className="ml-2">
+                  {member.name}
+                </List.Content>
+                <List.Content className="ml-auto">
+                  <Icon 
+                    className="remove-member-icon"
+                    role="button"
+                    onClick={() => removeMember(key)} 
+                    size="large" 
+                    name="trash" 
+                    color="red" />
+                </List.Content>
+              </List.Item>
+            )
+          })
+        }
+      </List>
+		)
+  }
+
+	static propTypes = {
+		members: PropTypes.array.isRequired,
+		removeMember: PropTypes.func.isRequired
+	}
+}
