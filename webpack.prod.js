b97de0b00989046bcaae4e7a922d1865c1a37d4a
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-06 07:54:17 +0300

    configured webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,16 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
+const path = require('path')
 
 module.exports = merge(common, {
   mode: "production",
+	output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.min.js'
+  },
   plugins: [
-    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
