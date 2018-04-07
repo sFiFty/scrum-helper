152984da2cf9a461dc33d5c76ece180147da4eec
sFiFty <alexandr.rudin.my@gmail.com>
2018-04-07 21:39:09 +0300

    started worked on burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Container, Image, Label} from 'semantic-ui-react'
+import {Container, Image, Label, Icon} from 'semantic-ui-react'
 import Navigation from 'Components/Navigation'
 import {Link} from 'react-router-dom'
 import Auth from 'Components/Auth'
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
     const {location} = this.props
     return (
       <header>
-        <Container fluid>
+        <Container fluid className="desktop-menu"> 
           <div className="d-flex justify-content-start">
             <div className="p-2">
               <div className="logo-container">
@@ -28,6 +28,9 @@ export default class Header extends React.Component {
             <div className="p-2 align-self-center"><Navigation location={location} /></div>
             <div className="ml-auto p-2 align-self-center"><Auth /></div>
           </div>
+        </Container>
+        <Container className="mobile-menu">
+          <Icon name="bars" />
         </Container>
       </header>
     )
