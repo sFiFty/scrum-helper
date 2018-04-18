0a96edc731123d704ac71f480d4728d19a3cab71
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-18 08:08:36 +0300

    added reset state to inline editable component

--- src/components/InlineEditable/InlineEditable.js
+++ src/components/InlineEditable/InlineEditable.js
@@ -6,16 +6,21 @@ export default class InlineEditable extends React.Component {
 
 	state = {
     text: this.props.text,
-    isEditing: false
+    isEditing: false,
+    value: this.props.text
 	}
 
   setText = event => this.setState({text: event.target.value})
   
-  hideEditing = () => this.setState({ isEditing: false })
+  hideEditing = () => this.setState({isEditing: false, text: this.state.value})
 
-  showEditing = () => this.setState({ isEditing: true })
+  showEditing = () => this.setState({isEditing: true})
+
+  onSave = text => {
+    this.props.onChange(text)
+    this.setState({value: text})
+  }
   
-	
 	render() {
     const {placeholder, onChange} = this.props
     const {text, isEditing} = this.state
@@ -24,7 +29,7 @@ export default class InlineEditable extends React.Component {
       <List.Content className="d-flex flex-row justify-content-between align-items-center">
         <Input size="mini" onChange={this.setText} value={text} placeholder={placeholder} />
         <div>
-          <Button secondary size="mini" onClick={() => onChange(text)}>Save</Button>
+          <Button secondary size="mini" onClick={() => this.onSave(text)}>Save</Button>
           <Button basic size="mini" onClick={this.hideEditing}>Cancel</Button>
         </div>
       </List.Content> :
