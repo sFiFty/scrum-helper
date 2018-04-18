d8131c0a359d6e210097e290a13a1eacd3bb49b6
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-18 17:10:14 +0300

    refactored contactUs page

--- src/screens/contactUs/ContactUs.js
+++ src/screens/contactUs/ContactUs.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import {Container, Button, Form, TextArea} from 'semantic-ui-react'
 
+const MAIL = 'alexandr.rudin.my@gmail.com'
+const FACEBOOK_LINK = 'https://www.facebook.com/rudinaleksandr'
+const LINKEDIN_LINK = 'https://www.linkedin.com/in/rudin-aleksandr-11921576/'
+
 export default class ContactUs extends Component {
 
   state = {
@@ -18,13 +22,13 @@ export default class ContactUs extends Component {
           If you have any questions or suggestions please don't hesitate to contact me.
         </div>
         <div className="socials mt-5">
-          <a target="_blank" href="mailto:alexandr.rudin.my@gmail.com">
+          <a target="_blank" href={`mailto:${MAIL}`}>
             <Button size="huge" circular color='google plus' icon='google plus' />
           </a>
-          <a target="_blank" href="https://www.facebook.com/rudinaleksandr">
+          <a target="_blank" href={FACEBOOK_LINK}>
             <Button className="ml-3" size="huge" circular color='facebook' icon='facebook' />
           </a>
-          <a target="_blank" href="https://www.linkedin.com/in/rudin-aleksandr-11921576/">
+          <a target="_blank" href={LINKEDIN_LINK}>
             <Button className="ml-3" size="huge" circular color='linkedin' icon='linkedin' />
           </a>
         </div>
