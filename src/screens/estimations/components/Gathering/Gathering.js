03e734497a0af5c51b1b5e87c04d68c30c33807f
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-28 08:43:28 +0300

    added responsive

--- src/screens/estimations/components/Gathering/Gathering.js
+++ src/screens/estimations/components/Gathering/Gathering.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {isLoaded, isEmpty} from 'react-redux-firebase'
 import SMLoader from 'Components/SMLoader'
-import {Container, Image} from 'semantic-ui-react'
+import {Container, Image, Button} from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import './styles.scss'
 
@@ -51,39 +51,42 @@ export default class Gathering extends Component {
 
   render() {
     const {joinedMembers, tasks, isOwner} = this.state
-    console.log(isOwner);
     return (
-      <Container className="gathering-container d-flex">
-        <div className="joined-members-container">
-          <h3> Joined members </h3>
-          <ul className="joined-members-list">
-            {
-              joinedMembers.map((member, i) => {
-                const memberAvatar = require(`Images/${member.avatar}`)
-                return <li key={i} className="d-flex align-items-center">
-                  <div className="member-image-box">
-                    <Image avatar src={memberAvatar} />
-                  </div>
-                  <div className="member-name">
-                    {member.name}
-                  </div>
-                </li>
-              })
-            }
-          </ul>
+      <Container className="gathering-container ">
+        <div className="box d-flex flex-column flex-md-row justify-content-center">
+          <div className="joined-members-container">
+            <h3> Joined members </h3>
+            <ul className="joined-members-list">
+              {
+                joinedMembers.map((member, i) => {
+                  const memberAvatar = require(`Images/${member.avatar}`)
+                  return <li key={i} className="d-flex align-items-center">
+                    <div className="member-image-box">
+                      <Image avatar src={memberAvatar} />
+                    </div>
+                    <div className="member-name">
+                      {member.name}
+                    </div>
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+          <div className="tasks-list-container">
+            <h3>Tasks to estimate</h3>
+            <ul className="tasks-list font-m">
+              {
+                tasks.map((task, i) => {
+                  return <li key={i} >
+                    {task.title}
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+          <Button secondary>Start estimation</Button>
         </div>
-        <div className="tasks-list-container">
-          <h3>Tasks to estimate</h3>
-          <ul className="tasks-list font-m">
-            {
-              tasks.map((task, i) => {
-                return <li key={i} >
-                  {task.title}
-                </li>
-              })
-            }
-          </ul>
-        </div>
+
       </Container>
     )
   }
