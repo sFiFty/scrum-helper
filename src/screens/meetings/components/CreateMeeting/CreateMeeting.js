ab57e485f20997c39f2d9177191650cb7d6ae481
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-31 08:39:15 +0300

    found and added icons to meetings type estimation and daily

--- src/screens/meetings/components/CreateMeeting/CreateMeeting.js
+++ src/screens/meetings/components/CreateMeeting/CreateMeeting.js
@@ -7,7 +7,19 @@ export default class CreateMeeting extends Component {
   render() {
     return (
       <Container className="create-meeting-container">
-        create meetings
+        <h1>Which type of the meeting you want to create?</h1>
+        <div className="meeting-types d-flex justify-content-center align-items-center">
+          <Link to="/daily/create">
+            <div className="create-meeting-box daily">
+              Create daily meeting
+            </div>
+          </Link>
+          <Link to="/estimation/create">
+            <div className="create-meeting-box estimation">
+              Create estimation meeting
+            </div>
+          </Link>
+        </div>
       </Container>
     )
   }
