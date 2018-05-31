75dc4df233d7189973781ff81797d3d92181dd63
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-31 08:11:13 +0300

    added create meeting box

--- src/screens/meetings/components/MeetingsList/Meetings.js
+++ src/screens/meetings/components/MeetingsList/Meetings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
 import { Container } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Meeting from '../Meeting'
 import './styles.scss'
@@ -58,6 +59,18 @@ export default class Meetings extends Component {
           meetings.length &&
           meetings.map(m => <Meeting key={m.key} uid={auth.uid} meeting={m} />)
         }
+        <Link to="meetings/create">
+          <div className="add-meeting-container d-flex flex-column justify-content-start align-items-center">
+            <div className="icon-container d-flex justify-content-center align-items-center">
+              <div className="icon-background d-flex justify-content-center align-items-center">
+                <i className="fas fa-plus"></i>
+              </div>
+            </div>
+            <div className="text font-l">
+              Add Meeting
+            </div>
+          </div>
+        </Link>
       </Container>
     )
   }
