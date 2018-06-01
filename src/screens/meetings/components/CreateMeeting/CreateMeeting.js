adc25ba2402ff0b1452d0e9f67653a401dd9c332
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-01 07:58:03 +0300

    finished adding meetings

--- src/screens/meetings/components/CreateMeeting/CreateMeeting.js
+++ src/screens/meetings/components/CreateMeeting/CreateMeeting.js
@@ -8,21 +8,25 @@ export default class CreateMeeting extends Component {
     return (
       <Container className="create-meeting-container">
         <h1>Which type of the meeting you want to create?</h1>
-        <div className="meeting-types d-flex justify-content-center align-items-center">
-          <div className="create-meeting-box">
+        <div className="meeting-types d-flex justify-content-center align-items-center text-center">
+          <div className="create-meeting-box d-flex flex-column justify-content-start">
             <Link to="/daily/create">
-              <div className="icon-container">
-                <img src={require('Images/daily-type-ico.svg')} alt="Daily type icon"/>
+              <div className="icon-container d-flex align-items-center justify-content-center">
+                <div className="icon-background d-flex align-items-center justify-content-center">
+                  <img src={require('Images/daily-type-ico.svg')} alt="Daily type icon"/>
+                </div>
               </div>
               <div className="font-m daily">
                 Create daily meeting
               </div>
             </Link>
           </div>
-          <div className="create-meeting-box">
+          <div className="create-meeting-box d-flex flex-column justify-content-start text-center">
             <Link to="/estimation/create">
-              <div className="icon-container">
-                <img src={require('Images/estimation-type-ico.svg')} alt="Estimation type icon"/>
+              <div className="icon-container d-flex align-items-center justify-content-center">
+                <div className="icon-background d-flex align-items-center justify-content-center">
+                  <img src={require('Images/estimation-type-ico.svg')} alt="Estimation type icon"/>
+                </div>
               </div>
               <div className="font-m estimation">
                 Create estimation meeting
