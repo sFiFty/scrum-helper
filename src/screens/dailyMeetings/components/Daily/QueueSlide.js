52faed1d1207fb32c73eb0be392decf3f3a01bf0
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-07-18 07:34:03 +0300

    added buttons

--- src/screens/dailyMeetings/components/Daily/QueueSlide.js
+++ src/screens/dailyMeetings/components/Daily/QueueSlide.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {List, Image} from 'semantic-ui-react'
+import {List, Image, Button} from 'semantic-ui-react'
 import ExtendMembersList from 'Helpers/ExtendMembersList'
 import PropTypes from 'prop-types'
 
@@ -9,6 +9,7 @@ export default class QueueSlide extends Component {
   state = {
     members: null,
     teamCard: null,
+    tasks: {},
   }
 
   componentWillMount() {
@@ -32,6 +33,14 @@ export default class QueueSlide extends Component {
   componentWillReceiveProps(nextProps) {
     const {daily} = nextProps
     this.setState({members: ExtendMembersList(daily.members, daily.team.members)})
+  }
+
+  markAsDone = (cardId) => {
+    console.log(cardId);
+  }
+
+  markAsOngoing = (cardId) => {
+    console.log(cardId);
   }
 
   generateTasks = (key, token) => {
@@ -59,7 +68,7 @@ export default class QueueSlide extends Component {
       }
       return status;
     })
-    tasks[initials] = notDoneCards[Math.floor(Math.random()*notDoneCards.length)].name;
+    tasks[initials] = notDoneCards[Math.floor(Math.random()*notDoneCards.length)];
     if (Object.keys(tasks).length === Object.keys(members).length) {
       resolve(tasks)
     }
@@ -83,7 +92,7 @@ export default class QueueSlide extends Component {
   render() {
     const {members, tasks, teamCard} = this.state
     const {daily} = this.props
-    console.log(daily.team)
+    console.log(tasks);
     return (
       <div key={daily.timestamp} style={{backgroundColor: daily.team.color}} className="page-overlay">
         <div className="daily-queue text-center">
@@ -91,7 +100,8 @@ export default class QueueSlide extends Component {
           <List className="queue-members">
             {
               _.keys(members).map((key, index) => {
-                let member = members[key]
+                const member = members[key]
+                const task = tasks[member.initials]
                 return (
                   <List.Item key={index}>
                     <Image avatar src={require(`Images/${member.avatar}`)} />
@@ -99,9 +109,11 @@ export default class QueueSlide extends Component {
                       <List.Header>{member.name}</List.Header>
                     </List.Content>
                     {
-                    tasks && tasks[member.initials] &&
+                      task &&
                       <div className="promise">
-                        <strong>Commitment: </strong>{tasks[member.initials]}
+                        <strong>Commitment: </strong>{task.name}
+                        <Button onClick={() => this.markAsDone(task.id)} size='mini' color='green'>Done</Button>
+                        <Button onClick={() => this.markAsOngoing(task.id)} size='mini' color='teal'>Ongoing</Button>
                       </div>
                     }
                   </List.Item>
