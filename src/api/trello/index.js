638dff0f74f17da5c1d56b19f95d3a30a172a318
sFiFty <alexandr.rudin.my@gmail.com>
2018-07-29 10:29:50 +0300

    finished trello api

--- src/api/trello/index.js
+++ src/api/trello/index.js
@@ -3,9 +3,19 @@ const token = '';
 
 export default {
   getBoardColumns: (boardId) => {
-    const url = `https://trello.com/1/boards/${boardId}/lists?fields=all&key=${key}&token=${token}`
-    return fetch(url).then((response) => {
-      return response.json();
-    });
-  }
+    const url = `https://trello.com/1/boards/${boardId}/lists?fields=all&key=${key}&token=${token}`;
+    return fetch(url).then(r => r.json());
+  },
+  getBoardLabels: (boardId) => {
+    const url = `https://api.trello.com/1/boards/${boardId}/labels?fields=all&key=${key}&token=${token}`;
+    return fetch(url).then(r => r.json());
+  },
+  markCardWithLabel: (labelId) => {
+    const url = `https://api.trello.com/1/cards/${cardId}?idLabels=${labelId}&key=${key}&token=${token}`;
+    return fetch(url, { method: 'PUT' });
+  },
+  getCardsByColumnId: (columnId) => {
+    const url = `https://trello.com/1/lists/${columnId}/cards?key=${key}&token=${token}`;
+    return fetch(url).then(r => r.json());
+  },
 }
