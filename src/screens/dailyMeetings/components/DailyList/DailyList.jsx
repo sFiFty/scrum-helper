be4f2e515a17bb05efc132fc1a9a6613105f25db
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-08-06 07:24:16 +0300

    finished daily list refactoring

--- src/screens/dailyMeetings/components/DailyList/DailyList.jsx
+++ src/screens/dailyMeetings/components/DailyList/DailyList.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {
-  Container, Header, List, Icon, Button,
+  Container, List, Icon, Button,
 } from 'semantic-ui-react/dist/commonjs';
 import { Link } from 'react-router-dom';
-import { isLoaded, isEmpty } from 'react-redux-firebase';
+import { isLoaded } from 'react-redux-firebase';
 import { NotificationManager } from 'react-notifications';
 import PropTypes from 'prop-types';
 
@@ -12,6 +12,19 @@ import MembersInTheList from 'Components/MembersInTheList';
 import AddListItemBox from 'Components/AddListItemBox';
 import ExtendMembersList from 'Helpers/ExtendMembersList';
 import './styles.scss';
+
+const propTypes = {
+  firebase: PropTypes.shape({
+    remove: PropTypes.func,
+  }).isRequired,
+  meetings: PropTypes.object.shape({
+    [PropTypes.string]: PropTypes.object,
+  }),
+};
+
+const defaultProps = {
+  meetings: null,
+};
 
 export default class DailyList extends Component {
   deleteDaily = (key) => {
@@ -24,19 +37,19 @@ export default class DailyList extends Component {
     });
   }
 
-	deleteMember = (member, meetingId) => {
-	  const { firebase, teams } = this.props;
-	  firebase.remove(`dailyMeetings/${meetingId}/members/${member.key}`).then(() => {
-	    NotificationManager.success(
-	      `Member ${member.name} successfully removed`,
-	      'Confirmation',
-	    );
-	  });
-	}
+  deleteMember = (member, meetingId) => {
+    const { firebase } = this.props;
+    firebase.remove(`dailyMeetings/${meetingId}/members/${member.key}`).then(() => {
+      NotificationManager.success(
+        `Member ${member.name} successfully removed`,
+        'Confirmation',
+      );
+    });
+  }
 
-	render() {
-	  const { meetings, teams } = this.props;
-	  return (
+  render() {
+    const { meetings } = this.props;
+    return (
       <Container className="list-container daily-list">
         <h2 className="list-title">My Daily Meetings</h2>
         {
@@ -44,15 +57,21 @@ export default class DailyList extends Component {
             ? (
               <List>
                 {
-                  _.keys(meetings).map((k) => {
+                  Object.keys(meetings).map((k) => {
                     const meeting = meetings[k];
-                    const extendedMembersList = ExtendMembersList(meeting.members, meeting.team.members);
+                    const extendedMembersList = ExtendMembersList(
+                      meeting.members, meeting.team.members,
+                    );
                     return (
                       <List.Item className="text-color item-container" key={k}>
                         <div className="color-filler" style={{ backgroundColor: meeting.team.color }} />
                         <List.Content>
                           <List.Header>{meeting.team.name} daily</List.Header>
-                          <MembersInTheList members={extendedMembersList} deleteMember={this.deleteMember} parent={k} />
+                          <MembersInTheList
+                            members={extendedMembersList}
+                            deleteMember={this.deleteMember}
+                            parent={k}
+                          />
                           <Button as={Link} to={`daily/ongoing/${k}`} className="join-button" inverted>Start</Button>
                           <div className="list-controls">
                             <Icon onClick={() => this.deleteDaily(k)} className="trash-icon" size="large" name="trash" color="red" />
@@ -67,12 +86,9 @@ export default class DailyList extends Component {
             ) : <SMLoader />
         }
       </Container>
-	  );
-	}
-
-	static propTypes = {
-	  firebase: PropTypes.object.isRequired,
-	  teams: PropTypes.object,
-	  meetings: PropTypes.object,
-	}
+    );
+  }
 }
+
+DailyList.propTypes = propTypes;
+DailyList.defaultProps = defaultProps;
