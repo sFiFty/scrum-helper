82f0c680108531803f1a9e35aa41be1b04700b17
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-08-07 07:36:38 +0300

    applied hoc to team profile

--- src/screens/teams/components/TeamProfile/TeamProfile.jsx
+++ src/screens/teams/components/TeamProfile/TeamProfile.jsx
@@ -7,11 +7,10 @@ import {
 import { CirclePicker } from 'react-color';
 
 import TeamMembers from '../AddTeam/TeamMembers.jsx';
-import SMLoader from 'Components/SMLoader';
 
 const propTypes = {
   owner: PropTypes.string.isRequired,
-  team: PropTypes.shape({
+  profileObj: PropTypes.shape({
     name: PropTypes.string,
     color: PropTypes.string,
   }).isRequired,
@@ -26,36 +25,30 @@ class TeamProfile extends Component {
   onChange = () => {};
 
   render() {
-    const { team } = this.props;
+    const { profileObj } = this.props;
+    const team = profileObj;
     const { isPopupOpen, memberAvatar, memberName } = this.state;
     return (
-      isLoaded(team)
-        ? (
-          <Container>
-            <Form className="add" id="add-team">
-              <Form.Field className="name">
-                <Input value={team.name} onChange={this.setName} size="massive" placeholder="Type team name here..." />
-              </Form.Field>
-              <Form.Field className="form-field">
-                <label className="label font-m">Pick your team color</label>
-                <CirclePicker name="color-picker" color={team.color} onChange={this.onPickColor} width="100%" circleSize={38} />
-              </Form.Field>
-            </Form>
-            <Form.Field className="form-field team-members">
-              <label htmlFor="team-members" className="label">Team members</label>
-              <TeamMembers
-                members={team.members}
-                memberAvatar={memberAvatar}
-                memberName={memberName}
-                isPopupOpen={isPopupOpen}
-              />
-            </Form.Field>
-          </Container>
-        ) : (
-          <Container>
-            <SMLoader />
-          </Container>
-        )
+      <Container>
+        <Form className="add" id="add-team">
+          <Form.Field className="name">
+            <Input value={team.name} onChange={this.setName} size="massive" placeholder="Type team name here..." />
+          </Form.Field>
+          <Form.Field className="form-field">
+            <label className="label font-m">Pick your team color</label>
+            <CirclePicker name="color-picker" color={team.color} onChange={this.onPickColor} width="100%" circleSize={38} />
+          </Form.Field>
+        </Form>
+        <Form.Field className="form-field team-members">
+          <label htmlFor="team-members" className="label">Team members</label>
+          <TeamMembers
+            members={team.members}
+            memberAvatar={memberAvatar}
+            memberName={memberName}
+            isPopupOpen={isPopupOpen}
+          />
+        </Form.Field>
+      </Container>
     );
   }
 }
--- src/screens/teams/components/TeamProfile/TeamProfileContainer.js
+++ src/screens/teams/components/TeamProfile/TeamProfileContainer.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firebaseConnect } from 'react-redux-firebase';
 
+import profilePageHoc from 'Containers/profilePageHoc.jsx';
 import TeamProfile from './TeamProfile.jsx';
 
 export default compose(
@@ -12,7 +13,7 @@ export default compose(
     },
   ]),
   connect(state => ({
-    team: state.firebase.data.team,
+    profileObj: state.firebase.data.team,
     owner: state.firebase.auth.uid,
   })),
-)(TeamProfile);
+)(profilePageHoc(TeamProfile));
