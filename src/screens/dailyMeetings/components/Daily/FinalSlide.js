fc6651c7b666461070fad505a78d82c1ef7e0f1d
sFiFty <alexandr.rudin.my@gmail.com>
2018-08-15 12:50:21 +0300

    refactored final slide

--- src/screens/dailyMeetings/components/Daily/FinalSlide.js
+++ src/screens/dailyMeetings/components/Daily/FinalSlide.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import Moment from 'react-moment';
 import PropTypes from 'prop-types';
+
+const propTypes = {
+  daily: PropTypes.shape({
+    team: PropTypes.shape({
+      color: PropTypes.string,
+    }),
+  }),
+};
 
 export default class FinalSlide extends Component {
   render() {
-    const { daily } = this.props;
     return (
-      <div style={{ backgroundColor: daily.team.color }} className="page-overlay">
+      <div style={{ backgroundColor: this.props.daily.team.color }} className="page-overlay">
         <div className="daily-text text-center">
-          <div>
-Well Done!
-          </div>
-          <div>
-Let`s get back to work
-          </div>
+          <div>Well Done!</div>
+          <div>Let`s get back to work</div>
         </div>
       </div>
     );
   }
-
-	static propTypes = {
-	  daily: PropTypes.object,
-	}
 }
+
+FinalSlide.propTypes = propTypes;
