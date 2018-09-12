5cde7bc71742be9bd903c7d2e91fa81a19445351
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-09-12 08:37:26 +0300

    continue working on trello ingration

--- src/screens/teams/components/TrelloIntegration/TrelloIntegration.jsx
+++ src/screens/teams/components/TrelloIntegration/TrelloIntegration.jsx
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Input, Form, Button } from 'semantic-ui-react';
+
+import './styles.scss';
 
 // const propTypes = {
 //   prop: PropTypes
 // }
 
 class TrelloIntegration extends Component {
+  state = {
+    trelloKey: null,
+  }
+
   onSetTrelloKey = value => this.setState({ trelloKey: value });
+
   render() {
+    const { trelloKey } = this.state;
     return (
-      <div>
-        <Form.Field className="trello-key">
-          <Input onChange={this.setTrelloKey} size="massive" placeholder="Type your trello key here..." />
+      <div className="trello-integration-container">
+        <Form.Field className="d-flex align-items-center trello-key">
+          <Input onChange={this.onSetTrelloKey} size="massive" placeholder="Type your trello key here..." />
+          {
+            trelloKey
+            && (
+              <Button onClick={this.addMember} className="ml-3" size="mini" secondary>
+                <span>Get boards</span>
+              </Button>
+            )
+          }
         </Form.Field>
       </div>
-    )
+    );
   }
 }
 
