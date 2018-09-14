90d2e7624ca28b17d4b7c9ff69d147c01b967a27
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-09-14 08:20:41 +0300

    implement boadrs selevtion

--- src/screens/teams/components/TrelloIntegration/TrelloIntegration.jsx
+++ src/screens/teams/components/TrelloIntegration/TrelloIntegration.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Input, Form, Button } from 'semantic-ui-react';
+import {
+  Input, Form, Button, Dropdown 
+} from 'semantic-ui-react';
 
 import './styles.scss';
 
@@ -10,23 +12,38 @@ import './styles.scss';
 
 class TrelloIntegration extends Component {
   state = {
-    trelloKey: null,
+    key: null,
+    token: null,
+    boards: null,
+    selectedBoardId: null,
+    simpleBoardsList: null,
   }
 
   onSetKey = (event, element) => this.setState({ key: element.value });
 
   onSetToken = (event, element) => this.setState({ token: element.value });
 
+  onChooseBoard = (event, data) => this.setState({ selectedBoardId: data.value })
+
   connect = () => {
     const { key, token } = this.state;
     const url = `https://api.trello.com/1/members/me/boards?key=${key}&token=${token}`;
     return fetch(url).then(response => response.json()).then((data) => {
-      console.log(data)
+      const simpleBoardsList = data.map(board => (
+        {
+          text: board.name,
+          value: board.id,
+          key: board.id,
+        }
+      ));
+      this.setState({ boards: data, simpleBoardsList });
     });
   }
 
   render() {
-    const { key, token } = this.state;
+    const {
+      key, token, simpleBoardsList, selectedBoardId,
+    } = this.state;
     return (
       <div className="trello-integration-container">
         <Form.Field className="d-flex align-items-center trello-key">
@@ -40,6 +57,18 @@ class TrelloIntegration extends Component {
             )
           }
         </Form.Field>
+        {
+          simpleBoardsList && (
+            <Dropdown
+              onChange={this.onChooseBoard}
+              placeholder="Select board"
+              value={selectedBoardId}
+              search
+              selection
+              options={simpleBoardsList}
+            />
+          )
+        }
       </div>
     );
   }
