f19da937dbd309f9fbaf885245623f6c1656ae0c
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-10-16 08:13:13 +0300

    continue working on team profile

--- src/screens/teams/components/TeamProfile/TeamProfile.jsx
+++ src/screens/teams/components/TeamProfile/TeamProfile.jsx
@@ -17,31 +17,59 @@ const propTypes = {
 
 class TeamProfile extends Component {
   state = {
+    team: null,
     isPopupOpen: false,
     memberAvatar: null,
     memberName: null,
   }
 
-  onPickColor = color => this.setState({ color: color.hex });
-  onSetName = event => this.setState({ name: event.target.value });
+  componentWillMount() {
+    const { profileObj } = this.props;
+    const members = profileObj.members ? Object.values(profileObj.members) : [];
+    this.setState({ team: profileObj, members });
+  }
 
-  onAddMember = (member) => console.log(member)
+  onPickColor = (color) => {
+    const { team } = this.state;
+    this.setState({ team: { ...team, color } });
+  };
+
+  onSetName = (event) => {
+    const { team } = this.state;
+    this.setState({ team: { ...team, name: event.target.value } });
+  };
+
+  onAddMember = (member) => {
+    const { members } = this.state;
+    members.push(member);
+    this.setState({ members });
+  }
+
   onRemoveMember = (member) => console.log(member)
 
   render() {
-    const { profileObj } = this.props;
-    const team = profileObj;
-    const members = team.members ? Object.values(team.members) : [];
+    const { team, members } = this.state;
     const { isPopupOpen, memberAvatar, memberName } = this.state;
     return (
       <Container>
         <Form className="profile" id="add-team">
           <Form.Field className="name">
-            <Input value={team.name} onChange={this.setName} size="massive" placeholder="Type team name here..." />
+            <Input
+              value={team.name}
+              onChange={this.onSetName}
+              size="massive"
+              placeholder="Type team name here..."
+            />
           </Form.Field>
           <Form.Field className="form-field">
             <label className="label font-m">Pick your team color</label>
-            <CirclePicker name="color-picker" color={team.color} onChange={this.onPickColor} width="100%" circleSize={38} />
+            <CirclePicker
+              name="color-picker"
+              color={team.color}
+              onChange={this.onPickColor}
+              width="100%"
+              circleSize={38}
+            />
           </Form.Field>
           <Form.Field className="form-field team-members">
             <label htmlFor="team-members" className="label">Team members</label>
