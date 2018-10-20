2c60f484d98d91e9144875f7d5b5f54b6b53033d
sFiFty <alexandr.rudin.my@gmail.com>
2018-10-20 17:26:48 +0300

    started refactoring on Daily component

--- src/screens/dailyMeetings/components/Daily/Daily.js
+++ src/screens/dailyMeetings/components/Daily/Daily.js
@@ -1,45 +1,20 @@
 import React, {Component} from 'react'
-import {isLoaded} from 'react-redux-firebase'
 import PropTypes from 'prop-types'
+
 import SMLoader from 'Components/SMLoader'
 import IntroSlide from './IntroSlide'
 import QueueSlide from './QueueSlide'
 import DiscussionSlide from './DiscussionSlide'
 import FinalSlide from './FinalSlide'
 import './styles.scss'
-import Divider from 'semantic-ui-react'
 
+const propTypes = {
+  dailyId: PropTypes.string.isRequired,
+  daily: PropTypes.object,
+  firebase: PropTypes.object.isRequired
+}
 
-const trelloKey = ''
-const trelloToken = ''
-const boardId = ''
-export default class Daily extends Component {
-  state = {
-    trelloColumns: null,
-    trelloLabels: null,
-  }
-  componentDidMount() {
-    this.getTrelloColumns();
-    this.getTrelloLabels();
-  }
-  getTrelloColumns = () => {
-    const url = `https://trello.com/1/boards/${boardId}/lists?fields=all&key=${trelloKey}&token=${trelloToken}`
-    return fetch(url).then((response) => {
-      return response.json();
-    }).then(data => {
-      this.setState({ trelloColumns: data });
-    })
-  }
-
-  getTrelloLabels = () => {
-    const url = `https://api.trello.com/1/boards/${boardId}/labels?fields=all&key=${trelloKey}&token=${trelloToken}`
-    return fetch(url).then((response) => {
-      return response.json();
-    }).then(data => {
-      this.setState({ trelloLabels: data });
-    })
-  }
-  
+class Daily extends Component {
   nextStep = e => {
     if (e.target.nodeName === 'BUTTON') return;
     const {daily, firebase, dailyId, history} = this.props
@@ -54,7 +29,7 @@ export default class Daily extends Component {
   }
 
   prevStep = () => {
-    const {daily, firebase, dailyId, history} = this.props
+    const {daily, firebase, dailyId} = this.props
     if (!daily) return
     if (daily.step === 0) return
     firebase.update(`dailyMeetings/${dailyId}`, { step: daily.step - 1 })
@@ -81,7 +56,7 @@ export default class Daily extends Component {
   }
 
   render() {
-    const { daily, history } = this.props
+    const { daily } = this.props
     const { trelloColumns, trelloLabels } = this.state;
     let currentSlide = <SMLoader />
     if (daily) {
@@ -90,7 +65,7 @@ export default class Daily extends Component {
           currentSlide = <IntroSlide {...this.props} />
           break
         case 1:
-          currentSlide =  <QueueSlide trelloLabels={trelloLabels} trelloColumns={trelloColumns} trelloKey={trelloKey} trelloToken={trelloToken} {...this.props} />
+          currentSlide =  <QueueSlide {...this.props} />
           break
         case 2:
           currentSlide =  <DiscussionSlide {...this.props} />
@@ -113,10 +88,8 @@ export default class Daily extends Component {
       </div>
     )
   }
-
-	static propTypes = {
-		dailyId: PropTypes.string.isRequired,
-		daily: PropTypes.object,
-		firebase: PropTypes.object.isRequired
-	}
 }
+
+Daily.propTypes = propTypes;
+
+export default Daily;
